4b4a2d4ebe92aa7e30c9a22f28fc989f11fad212
sickm0nkey <kbishwa03kalyan@gmail.com>
2022-09-13 14:49:43 +0530

    Task-2

---
+++ sextant/src/components/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Container() {
+    return (
+        <div className='container'>
+            <p>Container Area</p>
+        </div>
+    )
+}
+
+export default Container
---
+++ sextant/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/Title.css"
+
+function Title() {
+    return (
+        <div className='title'>
+            <h1>Sextant</h1>
+        </div>
+    )
+}
+
+export default Title
